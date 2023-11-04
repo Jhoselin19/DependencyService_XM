cd6d6b91979537d8f1a8ce906687182a636d7669
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-11-04 16:37:41 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio08/laboratorio08/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace laboratorio08
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ItemBattery.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BatteryDemo());
+            };
+            ItemQR.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new QrScanningDemo());
+            };
+        }
+    }
+}
